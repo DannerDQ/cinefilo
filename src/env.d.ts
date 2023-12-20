b1d04147e5a141ce35997e4792b01ff73542def7
DannerDQ <dannerdiazquispe2@gmail.com>
2023-12-20 15:23:06 -0500

    first commit

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,9 @@
 /// <reference types="astro/client" />
+interface ImportMetaEnv {
+    readonly API_KEY: string;
+    readonly API_TOKEN: string;
+}
+
+interface ImportMeta {
+    readonly env: ImportMetaEnv
+}
